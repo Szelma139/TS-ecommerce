57949cd3fc2d0689dc94edebcebc7e9144018f76
Szelma139 <raduniek1@gmail.com>
2021-05-16 17:28:44 +0200

    navbar simple

--- src/pages/LadningPage.tsx
+++ src/pages/LadningPage.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { CheckLatest } from '../sections/CheckLatest'
 import { CustomersSaying } from '../sections/CustomersSaying'
+import { Navbar } from '../sections/Navbar'
 import OurServices from '../sections/OurServices'
 
 export const LadningPage = () => {
     return (
         <div>
-            <CustomersSaying/>
+            <Navbar/>
             <CheckLatest/>
            <OurServices/> 
+           <CustomersSaying/>
         </div>
     )
 }
